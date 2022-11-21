ea2ecc608efe20cc50337a04d5dbbf56a4bf8c60
Liu Jiyuan <liujiyuan6@gmail.com>
2022-11-21 18:24:30 -0500

    commit

--- src/Main.java
+++ src/Main.java
@@ -5,8 +5,12 @@ public class Main {
 //        System.out.println("String: PAYPALISHIRING, row number:3  ");
 //        System.out.println(answer);
 
-        CharandInt test = new CharandInt();
-        int answer = test.method();
-        System.out.println(answer);
+//        CharandInt test = new CharandInt();
+//        int answer = test.method();
+//        System.out.println(answer);
+
+        StringToInt test = new StringToInt();
+        int result = test.myAtoi("100");
+        System.out.println(result);
     }
 }
